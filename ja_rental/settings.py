9bbe639637ca1046e6205840d5bb52d898f16258
LanaD-coder <c.wnt.nd1053@gmail.com>
2025-05-30 14:42:15 +0200

    change db to neon

--- ja_rental/settings.py
+++ ja_rental/settings.py
@@ -100,7 +100,7 @@ AUTHENTICATION_BACKENDS = (
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
