1eb9e344c435234e4d4b6736488aeed31842685f
LanaD-coder <c.wnt.nd1053@gmail.com>
2025-07-28 18:00:23 +0200

    set debug false

--- ja_rental/settings.py
+++ ja_rental/settings.py
@@ -24,7 +24,7 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # explicitly load .env
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True)
+DEBUG = env.bool('DEBUG', default=False)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='insecure-development-key')
