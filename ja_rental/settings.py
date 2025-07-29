be230f9d1566bcbcfe52c1f05a5d911ad41370cd
LanaD-coder <c.wnt.nd1053@gmail.com>
2025-07-29 10:36:52 +0200

    add static root to settings

--- ja_rental/settings.py
+++ ja_rental/settings.py
@@ -167,7 +167,7 @@ STATICFILES_DIRS = [
 ]
 
 # Where to collect all static files on deploy (run collectstatic)
-STATIC_ROOT = (BASE_DIR / 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
