fed1d28a65b9b48f01063aaaddf7842029d1867b
LanaD-coder <c.wnt.nd1053@gmail.com>
2025-07-29 10:02:46 +0200

    add bootstrap cards to tech data

--- ja_rental/settings.py
+++ ja_rental/settings.py
@@ -23,10 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # explicitly load .env
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True)
+DEBUG = False
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='insecure-development-key')
 
 # STRIPE Keys
