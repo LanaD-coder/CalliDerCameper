8751f9d72d4165fd015b61c621fcc2bae59540f6
LanaD-coder <c.wnt.nd1053@gmail.com>
2025-08-22 17:03:18 +0200

    Fix DATABASES config for Heroku

--- ja_rental/settings.py
+++ ja_rental/settings.py
@@ -109,14 +109,21 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
-# Only use Heroku Postgres
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+# If using environ.Env:
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    }
+else:
+    # fallback to local SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': str(BASE_DIR / "db.sqlite3"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
